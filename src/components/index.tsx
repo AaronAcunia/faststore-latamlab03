89de4dbe9f77d1c673bbd49cefc2e306c1a1bfb0
Aaron Acuña Alarcon <aacuna@medialabla.com>
2026-04-09 16:09:35 -0500

    changes

---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+import CallToAction from './CallToAction'
+import HelloWorld from './HelloWorld'
+import CustomHeader from './CustomHeader'
+
+export default { CallToAction, HelloWorld, CustomHeader }
