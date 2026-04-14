ba7fb6745b04d72236c2d9093a8d3975bf4d6946
Aaron Acuña Alarcon <aacuna@medialabla.com>
2026-04-14 15:28:36 -0500

    fix

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,7 +2,5 @@ import CallToAction from './CallToAction'
 import HelloWorld from './HelloWorld'
 import CustomHeader from './CustomHeader'
 import ProductShowcase from './ProductShowcase'
-import MultipleBanners from './MultipleBanners'
-import NanoBarSlider from './NanoBarSlider'
 
-export default { CallToAction, HelloWorld, CustomHeader, ProductShowcase, MultipleBanners, NanoBarSlider }
+export default { CallToAction, HelloWorld, CustomHeader, ProductShowcase }
